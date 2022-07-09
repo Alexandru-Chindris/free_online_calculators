f24caae5f4ce8e29294a10ae031d968eaf0d203d
Chindris Alexandru <alexandruioan.chindris@istitutoagnelli.it>
2022-07-10 00:23:25 +0200

    v:0.1

---
+++ centry_calculator.js
@@ -0,0 +1,16 @@
+function centuryCalculator() {
+    let input = document.getElementById("usr_input");
+    let result = document.getElementById("result");
+    let div = document.createElement("div");
+
+    if(input.value > 0){
+        let century = Math.ceil(input.value/100);
+        div.innerHTML="Century: " + century;
+        div.classList.add("result");
+        result.append(div);
+
+    }else{
+        alert("You must insert a positive value!");
+        input.value="";
+    } 
+}
