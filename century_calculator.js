b10b2f97e23e706be47a5d9faa437f074e657e3c
Chindris Alexandru <alexandruioan.chindris@istitutoagnelli.it>
2022-07-10 00:08:36 +0200

    updating code

--- century_calculator.js
+++ century_calculator.js
@@ -1,12 +1,14 @@
 function centuryCalculator() {
     let input = document.getElementById("usr_input");
-    let div = document.getElementById("loc");
-    let result = document.createElement("div");
+    let result = document.getElementById("result");
+    let div = document.createElement("div");
 
     if(input.value > 0){
         let century = Math.ceil(input.value/100);
-        result.innerHTML="Century: " + century;
-        div.append(result);
+        div.innerHTML="Century: " + century;
+        div.classList.add("result");
+        result.append(div);
+
     }else{
         alert("You must insert a positive value!");
         input.value="";
