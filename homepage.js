feba21d24f217ba4f1dddaa3e6631bfa5c35f2f8
Alexandru Chindris <alexandru.chindris.buisness@gmail.com>
2022-07-13 15:00:40 +0200

    1.1.0 stable version release

---
+++ homepage.js
@@ -0,0 +1,14 @@
+function century () {
+    window.location.href="century/century_calculator.html";
+}
+
+function github() {
+    window.location.href = "https://github.com/Alexandru-Chindris/web_century_calculator";
+}
+
+function abt_creator() {
+    let att = document.getElementById("footer");
+    let span1 = document.createElement("span");
+    span1.innerHTML="some uncompleted description :)";
+    att.append(span1);
+}
